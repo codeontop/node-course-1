d6edb51004bee9cfe28b93cb6081b9cb97d72d28
Tolulope Popoola <tolulopeontop@gmail.com>
2018-03-26 12:50:08 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require ('express');
+const hbs = require('hbs'); 
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) =>{
+ res.render('home.hbs', {
+     pageTitle: 'Home Page',
+     welcomeMessage: 'Welcome to my Website',
+     currentYear: new Date().getFullYear()
+ });
+});
+
+app.get('/about', (req, res) =>{
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+        currentYear: new Date().getFullYear()
+    })
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
